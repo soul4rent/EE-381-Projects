d72ddf44e65be5fa9e71c79ae7048dc6e6ef274a
soul4rent <kyle.a.wadsworth@gmail.com>
2018-01-24 11:27:34 -0800

    proj one

--- ProjectOne.py
+++ ProjectOne.py
@@ -1,1 +1,16 @@
-#general gist of random number generator
+# general gist of pseudorandom number generators
+# 1/24/2018
+# Exploration of pseudorandom number generators
+#
+# Below the parameters in the formula are assigned their values.
+
+mult = 3
+adder = 15
+norm = 11
+seed = 1
+
+#Below is the formula
+
+for x in range(1, 10):
+    seed = (mult * seed + adder)%norm
+    print(seed)
